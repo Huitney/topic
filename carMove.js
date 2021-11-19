68e52d69bd0dbe58aba662b00722f383e2b9a6fe
Huitney <wbang05@gmail.com>
2021-11-20 00:02:43 +0800

    ...

--- carMove.js
+++ carMove.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
-import {car, topCamera, thirdPVCamera, GPSCamera, keyboard, obstacles, RCmesh, carParameter, socket} from "./init.js";
+import {car, topCamera, thirdPVCamera, GPSCamera, keyboard, obstacles, RCmesh, carParameter} from "./init.js";
 import {PDControl} from "./func.js";
 
 var parkingMode = 0, parkingAngle = 0, PPart = 0;
